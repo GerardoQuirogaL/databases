4507d419747f0c5aabf81ece40084e9500613f90
Gerardo Luis Quiroga León <isc20350309@gmail.com>
2023-10-13 10:01:12 -0600

    validations to the endpoint that lists a user

--- controllers/users.js
+++ controllers/users.js
@@ -28,14 +28,25 @@ const listUsersByID = async (req = request, res = response) => {
     const {id} = req.params;
     let conn;
 
+    if (isNaN(id)){
+        res.status(400).json ({msg: `The ID ${id} is invalid`});
+        return;
+    }
+
     try{
         conn =  await pool.getConnection();
 
-        const user = await conn.query(usersModel.getByID, [id] ,(err)=>{
+        const [user] = await conn.query(usersModel.getByID, [id] ,(err)=>{
             if (err){
                 throw err;
             }
         })
+
+        if (!user){
+            res.status(404).json({msg: `User with ID ${id} not found`});
+            return;
+        }
+
         res.json(user);
     } catch (error){
         console.log(error);
@@ -47,4 +58,28 @@ const listUsersByID = async (req = request, res = response) => {
     }
 }
 
-module.exports = {listUsers, listUsersByID}
+const addUser = async (req = request, res = response) =>{
+    let conn;
+
+    try{
+        conn = await pool.getConnection();
+
+        const userAdded = await conn.query(usersModel.addRow,[user], (err)=>{
+            if (err) throw err;
+        })
+        
+        console.log(userAdded);
+        res.json(userAdded);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    } finally{
+        if (conn) conn.end();
+    }
+}
+
+module.exports = {listUsers, listUsersByID, addUser}
+
+
+
+// routes       controllers       models(DB)
